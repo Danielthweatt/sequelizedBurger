6975a326e2eaf868dcfafc1e7c2206ff5ead01ca
Danielthweatt <dthweatt192@yahoo.com>
2018-05-23 22:05:26 -0500

    worked on models

---
+++ models/burger.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes) {
+    var Burger = sequelize.define("burgers", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        burger_name: DataTypes.STRING,
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    },
+    {
+        timestamps: true,
+        updatedAt: false
+    });
+        return Todo;
+};
