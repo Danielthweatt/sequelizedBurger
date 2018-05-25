4bf079b83992f0e11bc10db77bcead69af2f2a47
Danielthweatt <dthweatt192@yahoo.com>
2018-05-25 11:49:24 -0500

    worked on sequelizing controller and index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,7 +8,6 @@ const router = express.Router();
 // Routes
 router.get("/", function(req, res){
     db.burgers.findAll({}).then(function(results){
-        console.log(results);
         const hbsObject = {
             burgers: results
         };
@@ -20,19 +19,24 @@ router.post("/api/burgers", function(req, res){
     db.burgers.create({
         burger_name: req.body.burger    
     }).then(function(result){
-        console.log(result.insertId);
-        res.json({id: result.insertId});
+        res.json({id: result.dataValues.id});
     });
 });
 
-// router.put("/api/burgers/:id", function(req, res){
-//     burger.updateOne({devoured: req.body.devoured}, {id: req.params.id}, function(result){
-//         if (result.changedRows === 0) {
-//             return res.status(404).end();
-//         }
-//         res.status(200).end();
-//     });
-// });
+router.put("/api/burgers/:id", function(req, res){
+    db.burgers.update({
+        devoured: req.body.devoured
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(function(result){
+        if (result === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
 
 // Export
 module.exports = router;
